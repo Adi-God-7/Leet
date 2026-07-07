26fab7f443e5f79b6b559a543e4510529192f416
Aditya Kumar <manorama.rajkumar@gmail.com>
2026-07-07 22:31:37 +0530

    Time: 0 ms (100%), Space: 10.9 MB (72.48%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.c
@@ -0,0 +1,17 @@
+int majorityElement(int* nums, int numsSize) {
+    int c = nums[0];
+    int count = 1;
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] == c) {
+            count++;
+        } 
+        else {
+            count--;
+            if (count == 0) {
+                c = nums[i];
+                count = 1;
+            }
+        }
+    }
+    return c;
+}
