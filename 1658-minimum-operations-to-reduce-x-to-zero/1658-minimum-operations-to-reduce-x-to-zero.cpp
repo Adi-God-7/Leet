452e62619bf02e3278f9ee469728684215f26289
Aditya Kumar <manorama.rajkumar@gmail.com>
2026-09-23 10:51:26 +0530

    Time: 4 ms (60.6%), Space: 102.3 MB (65.24%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    int minOperations(vector<int>& nums, int x) {        
-        int n    = nums.size();
-        int sum  = 0;
-        unordered_map<int, int> mp;
-        mp[0] = -1;
-        for(int i = 0; i<n; i++) {
-            sum += nums[i];
-            mp[sum] = i;
-        }
-        if(sum < x)
-            return -1;
-        int restSum  = sum-x;
-        int longest  = INT_MIN;
-        sum = 0;
-        for(int i = 0; i<n; i++) {
-            sum += nums[i];
-            if(mp.count(sum-restSum)) {
-                longest = max(longest, i-mp[sum-restSum]);
+    int minOperations(vector<int>& nums, int x) {
+        int cnt = 0;
+        int size = nums.size();
+        int l = 0;
+        int r = 0;
+        int sum = 0;
+        for(int i =0 ; i<nums.size(); i++) sum += nums[i];
+        int rmd = sum - x;
+        int maxsum = 0;
+        int maxlen = - 1;
+        if(rmd >= 0){
+            while(l<=r && r<=nums.size()){
+                if(maxsum < rmd){
+                    maxsum += nums[r];
+                    r++;
+                }
+                else if(maxsum > rmd){
+                    maxsum -= nums[l];
+                    l++;
+                }
+                if(maxsum == rmd){
+                    if((r-l)>=maxlen){
+                        maxlen = r - l;
+                    }
+                    maxsum -= nums[l];
+                    l++;
+                }
             }
         }
+        else return -1;
+        if(maxlen==-1) return -1;
+        else return nums.size() - maxlen;
+
         
-        return longest==INT_MIN?-1:n-longest;
     }
 };
